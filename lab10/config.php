f49114b013f9dd1f6c4a9d9f8b9f02a78efd7995
MaydaySun <61482640+MaydaySun@users.noreply.github.com>
2020-06-07 16:13:11 +0800

    add lab10

---
+++ lab10/config.php
@@ -0,0 +1,7 @@
+<?php
+define('DBHOST', 'localhost');
+define('DBNAME', 'art');
+define('DBUSER', 'testuser');
+define('DBPASS', '.20010727bth.');
+define('DBCONNSTRING','mysql:dbname=art;charset=utf8mb4;');
+?>
